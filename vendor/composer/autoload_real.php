dcb6babbbebce24e2f36ece1ab4615055194c0ed
adi <adityabhat@lazaro.in>
2018-12-03 14:49:59 +0530

    Cost Estimator : Add in PHPSpreadsheet

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit9f88672c827d72623ac6301c89ef43a5
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit9f88672c827d72623ac6301c89ef43a5::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire9f88672c827d72623ac6301c89ef43a5($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire9f88672c827d72623ac6301c89ef43a5($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
