c53e9c19ca4da27f71afd3d6e615a2ca0781cef7
adi <adityabhat@lazaro.in>
2018-12-07 11:43:30 +0530

    SEO : Add Google Site Verification

--- inc/lazaro-hooks.php
+++ inc/lazaro-hooks.php
@@ -6,6 +6,9 @@
 add_action( 'wp_head', function () {
 	?>
 
+		<!-- Google Site Verification -->
+		<meta name="google-site-verification" content="ahJnQ8d_fmOtmfxLC1VbW-GfHfVC4GqFd5W252bj55M" />
+
 		<!-- Google Tag Manager -->
 		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-53BHH8G');</script>
 		<!-- End Google Tag Manager -->
