c308c50a8be0f1e19e3dccfcdc244e4c0857e6a2
adi <adityabhat@lazaro.in>
2019-01-24 11:14:04 +0530

    REVERT\ "Tracking / Analytics : Add LinkedIn"
    This reverts commit 216884fd0127ced9e5268c398e25e714653c737c.

--- inc/lazaro-hooks.php
+++ inc/lazaro-hooks.php
@@ -46,18 +46,6 @@ add_action( 'wp_footer', function () {
 		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53BHH8G" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<!-- End Google Tag Manager (noscript) -->
 
-		<!-- LinkedIn -->
-		<script type="text/javascript">
-			_linkedin_partner_id = "339747"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-		</script>
-		<script type="text/javascript">
-			(function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})();
-		</script>
-		<noscript>
-			<img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=339747&fmt=gif" />
-		</noscript>
-		<!-- End: LinkedIn -->
-
 	<?php
 } );
 
